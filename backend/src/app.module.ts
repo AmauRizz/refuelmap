4ddeb1b26dba5ec1727126b9d4c27a967ee8a599
AmauRizz <amaury.mulcey.pro@gmail.com>
2026-04-25 03:53:04 +0200

    Add config env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [
-      CacheModule.register({
-        isGlobal: true,
-        ttl: 300_000,
-      }),
-  ],
-  controllers: [AppController],
-  providers: [
-      AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        CacheModule.registerAsync({
+            isGlobal: true,
+            imports: [ConfigModule],
+            useFactory: (config: ConfigService) => ({
+                ttl: config.get<number>('CACHE_TTL', 300_000),
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CacheInterceptor,
+        },
+    ],
 })
 export class AppModule {}
